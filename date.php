bc6f7b4f2762ad571212903d181446415b2bc833
Yukamiro <yukamiro2@gmail.com>
2025-02-28 12:22:06 +0100

    First commit

---
+++ date.php
@@ -0,0 +1,57 @@
+<?php
+$boissons = [
+    [
+        "type" => "café",
+        "nom" => "café expresso",
+        "prix" => 2,
+        "sucre" => true,
+    ],
+    [
+        "type" => "café",
+        "nom" => "café long",
+        "prix" => 1,
+        "sucre" => true,
+    ],
+    [
+        "type" => "café",
+        "nom" => "cappuccino",
+        "prix" => 0.5,
+        "sucre" => true,
+    ],
+    [
+        "type" => "café",
+        "nom" => "Latte macchiato",
+        "prix" => 1.5,
+        "sucre" => true,
+    ],
+    [
+        "type" => "café",
+        "nom" => "Chocolat chaud",
+        "prix" => 2,
+        "sucre" => true,
+    ],
+    [
+        "type" => "café",
+        "nom" => "Thé vert",
+        "prix" => 1,
+        "sucre" => true,
+    ],
+    [
+        "type" => "café",
+        "nom" => "Infusion verveine",
+        "prix" => 3,
+        "sucre" => true,
+    ],
+    [
+        "type" => "café",
+        "nom" => "Mocaccino",
+        "prix" => 1,
+        "sucre" => true,
+    ],
+    [
+        "type" => "café",
+        "nom" => "Thé noir",
+        "prix" => 0.5,
+        "sucre" => true,
+    ]
+];

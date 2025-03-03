f7193bad7d730c5364e0b1261bdaa10296312d8a
Yukamiro <yukamiro2@gmail.com>
2025-03-03 14:16:47 +0100

    commit

--- choixSucre.php
+++ choixSucre.php
@@ -1,11 +1,29 @@
 <?php
+require_once "data.php";
 var_dump($_GET);
-var_dump($_POST)
+var_dump($_POST);
+
+
+if (isset($_GET["boisson"]) == true) {
+
+    foreach ($boissons as $boisson) {
+
+        if ($_GET["boisson"] == $boisson["nom"]) {
+            $boissonChoisis = $boisson["nom"];
+        }
+    }
+} else {
+    header("location: choixBoisson.php");
+}
+
 ?>
 
-<form method= "POST" action="confirmCommande.php">
-    <label for="sucre"></label>
-<input type="number" id="sucre" name="sucre" value="0">
+<p> Vous avez choisie <?php echo ($boissonChoisis) ?></p>
 
+<form method="POST" action="confirmCommande.php?boisson=<?php echo ($_GET["boisson"]) ?>">
+    <label for="sucre"> Combien de sucre ? (0 => 5)</label>
+    <input type="number" id="sucre" name="sucre" value="0" min="0" max="5">
+    <label for="submit"></label>
+    <input type="submit" value="Envoyer">
 
 </form>
--- confirmCommande.php
+++ confirmCommande.php
@@ -1,4 +1,18 @@
 <?php
+require_once "data.php";
 
 var_dump($_GET);
 var_dump($_POST);
+
+if (isset($_POST["nom"]) == true) {
+    if (isset($_POST["sucre"]) == true) {
+
+        foreach ($boissons as $boisson) {
+            echo ($boisson["sucre"]);
+
+            if ($_GET["boisson"] == $boisson["nom"]) {
+                $boissonChoisis = $boisson["nom"];
+            }
+        }
+    }
+}

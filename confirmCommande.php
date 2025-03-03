8c3c3c67587d147850ed24075520a7c40582aacf
Yukamiro <yukamiro2@gmail.com>
2025-03-03 14:48:15 +0100

    final commit

--- confirmCommande.php
+++ confirmCommande.php
@@ -4,15 +4,41 @@ require_once "data.php";
 var_dump($_GET);
 var_dump($_POST);
 
-if (isset($_POST["nom"]) == true) {
+if (isset($_GET["boisson"]) == true) {
+
     if (isset($_POST["sucre"]) == true) {
 
-        foreach ($boissons as $boisson) {
-            echo ($boisson["sucre"]);
 
-            if ($_GET["boisson"] == $boisson["nom"]) {
-                $boissonChoisis = $boisson["nom"];
-            }
+        $nombreSucre = $_POST["sucre"];
+        if ($nombreSucre >= 0 and $nombreSucre <= 5) {
+?>
+            <p> Voici votre récapitulatif :
+                <?php echo ($_GET["boisson"]) ?> avec
+                <?php
+                echo ($nombreSucre);
+                if ($nombreSucre >= 2) {
+                    echo (" sucres");
+                } else {
+                    echo (" sucre");
+                } ?> </p>
+
+            <form method="POST" action="finCommande.php">
+                <label for="submit"></label>
+                <input type="submit" value="Valider la commande">
+            </form>
+            <form method="POST" action="choixBoisson.php">
+                <label for="submit"></label>
+                <input type="submit" value="Annuler ma commande">
+            </form>
+<?php
+
+        } else {
+            header("location: choixBoisson.php");
         }
+    } else {
+
+        header("location: choixBoisson.php");
     }
+} else {
+    header("location: choixBoisson.php");
 }
